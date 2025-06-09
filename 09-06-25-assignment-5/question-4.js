3d2a2634199f2582c46772658400a29d747b39b1
Geetanjali Nayak <geetanjali.nayak@geetanjaliss427.successivetech.com>
2025-06-09 17:23:20 +0530

    feat:added question 4

--- 09-06-25-assignment-5/question-4.js
+++ 09-06-25-assignment-5/question-4.js
@@ -1,20 +1,30 @@
 // Q4. Write a program deepClone that takes an object as input and returns a deep copy of that object. The function should handle nested objects and arrays.
 
-const originalObject={
-    name:"John",
-    age:34,
-    id:1,
+const originalObject = {
+  name: "John",
+  age: 34,
+  id: 1,
+  adrress:{
+    city:"New Delhi",
+    country:"India",
+  }
+};
+
+function deepClone(obj) {
+  newObject = {};
+
+  for (const properties in obj) {
+    newObject[properties] = obj[properties];
+  }
+
+  return newObject;
 }
 
-function deepClone(originalObject){
-    Object.create(newObject);
+let obj1 = deepClone(originalObject);
 
-    for(const properties in originalObject){
-        newObject.name= "Mike";
-        newObject.age=34,
-        newObject.id=1;
-    }
+obj1.age = 32;
 
-    return newObject;
-}
 
+
+console.log(originalObject); // Creates deep copy
+console.log(obj1);
