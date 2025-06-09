f56b01d0934a05fb226805d7ebcdab4fde1d9d0f
Geetanjali Nayak <geetanjali.nayak@geetanjaliss427.successivetech.com>
2025-06-09 16:24:20 +0530

    fix:Used filter instead of loop

--- 05-06-25-assignment-4/question-12.js
+++ 05-06-25-assignment-4/question-12.js
@@ -1,33 +1,21 @@
 //12. Write a program to display intersection of two array
 
-function intersectionOfArray(arr1,arr2)
-{
-    let len1=arr1.length;
-    let len2 = arr2.length;
+function intersectionOfArray(arr1, arr2) {
+  let len1 = arr1.length;
+  let len2 = arr2.length;
 
-    const newSet=new Set();
+  const newSet = new Set();
 
-    for(let i=0;i<len1;i++)
-    {
-        newSet.add(arr1[i]);
-    }
+  for (let i = 0; i < len1; i++) {
+    newSet.add(arr1[i]);
+  }
 
-    let resArr=[];
-    for(let i=0;i<len1;i++)
-    {
-        if(newSet.has(arr2[i]))
-        {
-            resArr.push(arr2[i]);
-        }
-    }
+  const resArr = arr2.filter((val) => newSet.has(val));
 
-    return resArr;
+  return resArr;
+}
 
-} 
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [1, 2, 3, 4];
 
-
-
-let arr1=[1,2,3,4,5];
-let arr2=[1,2,3,4];
-
-console.log(`Intersection of two array: ${intersectionOfArray(arr1,arr2)}`);
+console.log(`Intersection of two array: ${intersectionOfArray(arr1, arr2)}`);
