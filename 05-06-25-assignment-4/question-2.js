d293ee139cdbcd1cadd81fad8b0be096eb325691
Geetanjali Nayak <geetanjali.nayak@geetanjaliss427.successivetech.com>
2025-06-09 17:57:30 +0530

    fix:removed for loop

--- 05-06-25-assignment-4/question-2.js
+++ 05-06-25-assignment-4/question-2.js
@@ -1,38 +1,12 @@
 //2.Write a program to reverse a string ("Hello John" => "olleH nhoJ")
 
-const reversedString = (str)=>{
-    let n=str.length;
-    let splitString = str.split(" ");
-    let resString="";
+const reversedString = (str) => {
+  const reversed = str.split(" ").map((word) => word.split("").reverse().join("")).join(" ");
 
-    for(let i=0;i<splitString.length;i++)
-    {
-      // let str1=splitString[0];
-       resString+=reverseWord(splitString[i])+" ";
-    }
-
-    return resString;
-
-}
+  return reversed;
+};
 
 
-function reverseWord(word)
-{
-   let stack=[];
-   for(let i=0;i<word.length;i++)
-   {
-    stack.push(word[i]);
-   }
-    let newStr=[];
-   for(let j=stack.length-1;j>=0;j--)
-   {
-     newStr.push(stack[j]);
-   }
-
-   return newStr.join('');
-}
-
-
-let str="Hello John";
+let str = "Hello John";
 
 console.log(`Reversed String: ${reversedString(str)}`);
