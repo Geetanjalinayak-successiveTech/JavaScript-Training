0822d6cc262410d054a3e6454eac80980154d5c2
Geetanjali Nayak <geetanjali.nayak@geetanjaliss427.successivetech.com>
2025-06-16 22:53:38 +0530

    fix: correct the output of  question 4,5,8

--- 05-06-25-assignment-4/question-14.js
+++
@@ -1,5 +0,0 @@
-// Q-14 Write a program to reverse an array
-
-let arr=[1,2,3,4,5];
-const reversedArray=arr.reverse();
-console.log(`Reversed Array: ${reversedArray}`);
--- Question_4.js
+++
@@ -1,8 +0,0 @@
-//question 4- Write a function expression that takes in a number and returns its square.
-
-
-function squareNumber(number){
-    return number**2;
-}
-console.log(squareNumber(2));
-
