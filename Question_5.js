d9c4dec195bd4d4f434a9199cf29215bb7fa818e
Geetanjali Nayak <geetanjali.nayak@geetanjaliss427.successivetech.com>
2025-06-16 23:26:39 +0530

    Add node_modules to .gitignore

--- Question_5.js
+++
@@ -1,8 +0,0 @@
-// Write a function expression that takes in two numbers and returns their sum.
-
-function sumOfTwoNumbers(num1,num2)
-{
-    return num1+num2;
-}
-
-console.log(sumOfTwoNumbers(2,4));
